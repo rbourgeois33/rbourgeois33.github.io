5e1382c6350566a6e626cc3af2f5edfaf88db4c5
Remi Bourgeois <Remi.Bourgeois@cea.fr>
2025-09-03 11:15:35 +0200

    add sample-3

--- code-sample/sample-3.cpp
+++ code-sample/sample-3.cpp
@@ -5,11 +5,12 @@ int main(int argc, char* argv[]) {
     Kokkos::initialize(argc, argv);
     {
         int N = 10000; 
-        Kokkos::View<float**, Kokkos::LayoutLeft>  A_LL("A_LL", N, N);       
+        Kokkos::View<float**, Kokkos::LayoutLeft>   A_LL("A_LL", N, N);      
+        Kokkos::View<float**, Kokkos::LayoutRight>  A_LR("A_LR", N, N);       
         Kokkos::View<float*> row_sum("row_sum", N); 
-        Kokkos::View<float*> col_sum("col_sum", N); 
 
         Kokkos::deep_copy(A_LL, 1);
+        Kokkos::deep_copy(A_LR, 1);
 
         Kokkos::parallel_for("RowSumLL", N, KOKKOS_LAMBDA(int i) {
             float sum = 0.0;
@@ -18,12 +19,13 @@ int main(int argc, char* argv[]) {
             }
             row_sum(i) = sum;
         });
-        Kokkos::parallel_for("ColSumLL", N, KOKKOS_LAMBDA(int j) {
+
+        Kokkos::parallel_for("RowSumLR", N, KOKKOS_LAMBDA(int i) {
             float sum = 0.0;
-            for (int i = 0; i < N; i++) {
-                sum += A_LL(i,j);
+            for (int j = 0; j < N; j++) {
+                sum += A_LR(i,j);
             }
-            col_sum(j) = sum;
+            row_sum(i) = sum;
         });
     }
     Kokkos::finalize();
