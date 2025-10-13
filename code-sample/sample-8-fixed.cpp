8cbbb0e47f8772b661b2918d718c318f024ccb17
Remi Bourgeois <Remi.Bourgeois@cea.fr>
2025-10-13 11:20:21 +0200

    add sample 8

---
+++ code-sample/sample-8-fixed.cpp
@@ -0,0 +1,31 @@
+#include <Kokkos_Core.hpp>
+
+int main(int argc, char* argv[]) {
+  Kokkos::initialize(argc, argv);
+  {
+
+    const int size = 1<<27;
+
+    Kokkos::View<float*> A("A", size);
+
+    Kokkos::deep_copy(A, 1.0f);
+
+    float result = 0;
+    float dx = 1.0f/size;
+
+    Kokkos::parallel_reduce("Kernel", size, KOKKOS_LAMBDA(const int i, float& lsum) { 
+      
+      float x = ((float) i)*dx;
+
+      if ( i<size/2 ){
+        lsum += x ;
+      }else{
+        lsum += Kokkos::cosh(x);
+        lsum -= Kokkos::sinh(x);
+      }
+
+    }, result);
+
+  }
+  Kokkos::finalize();
+}
