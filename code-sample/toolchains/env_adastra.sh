c7d52b0cdc5e93d6911f048670e222bbf8d17941
rbourgeois33 <remi.bourgeois33@gmail.com>
2026-03-27 12:02:12 +0100

    add MI300A workloads

--- code-sample/toolchains/env_adastra.sh
+++ code-sample/toolchains/env_adastra.sh
@@ -4,4 +4,5 @@ cd $WORK
 module use rocm
 module load python rocm-7.2.0
 source venv_rocprof_compute/bin/activate
+export HSA_XNACK=1
 cd rbourgeois33.github.io/code-sample/
---
+++ code-sample/toolchains/hip.mi300a.cmake
@@ -0,0 +1,5 @@
+set(CMAKE_C_COMPILER amdclang)
+set(CMAKE_CXX_COMPILER amdclang++)
+
+set(Kokkos_ENABLE_HIP ON)
+set(Kokkos_ARCH_AMD_GFX942_APU ON)
